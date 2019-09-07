2d38038e7db9be933fb2fb5426d1a0ac32295e83
luomingx <381493362@qq.com>
2019-09-07 13:42:13 +0800

    first commit

---
+++ config/conf.py
@@ -0,0 +1,10 @@
+GY_API_URL = 'http://dev.guoyasoft.com:8080'
+
+GY_DB_URL = {                               
+    'host': 'qa.guoyasoft.com',             
+    'port': 3306,                           
+    'db': 'guoya_virtual_mall_1811',        
+    'user': 'root',                         
+    'passwd': 'Guoya006',                   
+    'charset': 'utf8'                       
+}											
---
+++ test_case/test_carefree/test_carefree_01.py
@@ -0,0 +1,12 @@
+from tools import request_tool
+from tools import assert_tool
+from tools import random_tool
+from tools import mysql_tool
+from tools import excel_tool
+from tools import log_tool
+from tools import os_tool
+from tools import shell_tool
+import pytest
+import allure
+# 项目根目录建config包，里面建conf.py文件，用于配置
+from config import conf
